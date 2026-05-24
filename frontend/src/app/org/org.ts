20523602f53c6b5659f9e41e9869f00b7edf38ae
03milosevicN <nikolamilosevic220803@gmail.com>
2026-05-24 14:24:25 +0200

    frontend: Finished User component.

--- frontend/src/app/org/org.ts
+++ frontend/src/app/org/org.ts
@@ -1,8 +1,18 @@
 import { Component } from "@angular/core";
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {LucideCalendarClock, LucideCreditCard, LucideUsers, LucideWalletCards} from "@lucide/angular";
 
 @Component({
   selector: "app-org",
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    LucideUsers,
+    LucideCreditCard,
+    LucideWalletCards,
+    LucideCalendarClock
+  ],
   templateUrl: "./org.html",
   styleUrl: "./org.css",
 })
