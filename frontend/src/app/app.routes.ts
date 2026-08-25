a277405ca894720b1abfbb6eaf09faf4b7241d67
03milosevicN <nikolamilosevic220803@gmail.com>
2026-08-25 18:29:12 +0200

    frontend: add necessary component routes, interceptor configuration, deps.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Register } from './auth/register';
+import { Login } from './auth/login';
+import { User } from './user/user';
+import { Org } from './org/org';
+import { Board } from './board/board';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
+  { path: 'u/:userId', component: User },
+  { path: 'org/:orgId', component: Org },
+  { path: 'b/:boardId', component: Board },
+];
